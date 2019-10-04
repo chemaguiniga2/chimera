347588eda7d23dad7e36150dd9f44636614c7c1c
chemaguiniga2 <A01376669@itesm.mx>
2019-10-04 17:55:28 -0500

    firstOfOperator

--- Parser.cs
+++ Parser.cs
@@ -1,1 +1,15 @@
 //Hola soy Dach 
+
+        static readonly ISet<TokenCategory> firstOfOperator =
+            new HashSet<TokenCategory>() {
+                TokenCategory.AND,
+                TokenCategory.OR,
+                TokenCategory.XOR,
+                TokenCategory.DIV,
+                TokenCategory.REM,
+                TokenCategory.NOT,
+                TokenCategory.EQUAL,
+                TokenCategory.INEQUAL,
+                TokenCategory.LESSTHAN,
+                TokenCategory.BIGGERTHAN,
+            };
