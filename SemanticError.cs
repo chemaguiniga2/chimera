db1fb4a850aeac91e953d997b78aba088de13be3
DacheOA <dachely.otero.argote@gmail.com>
2019-11-08 18:23:28 -0600

    base entrega 4

---
+++ SemanticError.cs
@@ -0,0 +1,22 @@
+/*
+Dachely Otero Argote            A01377812
+José María Aguíñiga Díaz        A01376669 
+José Rodrigo Narváez Berlanga   A01377566
+ */
+
+using System;
+
+namespace Chimera {
+
+    class SemanticError: Exception {
+
+        public SemanticError(string message, Token token):
+            base(String.Format(
+                "Semantic Error: {0} \n" +
+                "at row {1}, column {2}.",
+                message,
+                token.Row,
+                token.Column)) {
+        }
+    }
+}
---
+++ Type.cs
@@ -0,0 +1,14 @@
+/*
+Dachely Otero Argote            A01377812
+José María Aguíñiga Díaz        A01376669 
+José Rodrigo Narváez Berlanga   A01377566
+ */
+
+ using System;
+
+namespace Chimera {
+
+    public enum Type {
+        // agregar nuestros tipos
+    }
+}
